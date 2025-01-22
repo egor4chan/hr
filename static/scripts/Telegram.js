b1a8d033d171154c85c737849ce299d14669c737
egor4chan <93943877+egor4chan@users.noreply.github.com>
2025-01-22 18:34:26 +0300

    Add files via upload

--- static/scripts/Telegram.js
+++ static/scripts/Telegram.js
@@ -1,26 +1,17 @@
 var WebApp = window.Telegram.WebApp;
 
+var userTelegamID = WebApp.initDataUnsafe.user.id;
+var userReferID = WebApp.initDataUnsafe.start_param;
+var personalLink = 'https://t.me/share/url?url=t.me/hopterbigbot?startapp=' + userTelegamID;
+
+
 function vibration() {
     WebApp.HapticFeedback.impactOccurred('light');
 }
 
-var userTelegamID = WebApp.initDataUnsafe.user.id;
-var userReferID = WebApp.initDataUnsafe.start_param;
-var personalLink = 't.me/share/url?url=t.me/hopterbigbot?startapp=' + userTelegamID;
+function invite() {
+    vibration()
+    window.location.href = personalLink;
+}
 
 
-document.querySelector('#copy').addEventListener('click', e => {
-    vibration();
-    var inp = document.createElement('input')
-    inp.value = "Hi! I'm copied by button"
-    document.body.appendChild(inp)
-    inp.select()
-    
-    if (document.execCommand('copy')) {
-      console.log("Done!")
-    } else {
-      console.log("Failed...")
-    }
-    
-    document.body.removeChild(inp)
-  })
