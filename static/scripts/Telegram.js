d95ced04e79c1807e04334c75b9f38ff9b15648d
egor4chan <93943877+egor4chan@users.noreply.github.com>
2025-01-22 19:18:54 +0300

    Add files via upload

--- static/scripts/Telegram.js
+++ static/scripts/Telegram.js
@@ -1,20 +1,16 @@
 
 var WebApp = window.Telegram.WebApp;
-alert('Webapp success!')
+alert('Webapp !')
 var userTelegamID = WebApp.initDataUnsafe.user.id;
 alert(`UID is working: ${userTelegamID}`)
 //var userReferID = WebApp.initDataUnsafe.start_param;
 var personalLink = `https://t.me/share/url?url=t.me/hopterbigbot?startapp=${userTelegamID}`;
 alert(`Perslink is working: ${personalLink}`)
 
-function vibration() {
-    var WebApp = window.Telegram.WebApp;
-    WebApp.HapticFeedback.impactOccurred('light');
-}
 
-function invite() {
-    vibration()
-    window.location.href = personalLink;
+function getInvite() {
+    WebApp.HapticFeedback.impactOccurred('light');
+    WebApp.openLink(personalLink);
 }
 
 
