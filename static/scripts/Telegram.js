a53b4cbad0f1d3da562cdf64fbcc0e145ceb17dc
egor4chan <93943877+egor4chan@users.noreply.github.com>
2025-01-22 18:29:04 +0300

    Add files via upload

--- static/scripts/Telegram.js
+++ static/scripts/Telegram.js
@@ -1,4 +1,26 @@
+var WebApp = window.Telegram.WebApp;
+
 function vibration() {
-    var WebApp = window.Telegram.WebApp;
     WebApp.HapticFeedback.impactOccurred('light');
 }
+
+var userTelegamID = WebApp.initDataUnsafe.user.id;
+var userReferID = WebApp.initDataUnsafe.start_param;
+var personalLink = 't.me/share/url?url=t.me/hopterbigbot?startapp=' + userTelegamID;
+
+
+document.querySelector('#copy').addEventListener('click', e => {
+    vibration();
+    var inp = document.createElement('input')
+    inp.value = "Hi! I'm copied by button"
+    document.body.appendChild(inp)
+    inp.select()
+    
+    if (document.execCommand('copy')) {
+      console.log("Done!")
+    } else {
+      console.log("Failed...")
+    }
+    
+    document.body.removeChild(inp)
+  })
