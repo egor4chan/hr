c415b3be71dfaa4e1d20001411d1f51f611d85f5
egor4chan <93943877+egor4chan@users.noreply.github.com>
2025-01-22 19:38:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sql 
 
 app = Flask(__name__)
+data = sql.Data()
 
 @app.route('/')
 def mainPage():
     return render_template('index.html')
 
-# Hello via
+@app.route('/auth', methods=['POST'])
+def auth_user():
+    req = request.get_json(force=True, silent=True)
+
+    try:
+        user_id = int(req['user_id'])
+        refer_id = int(req['refer_id'])
+
+        data.Auth(user_id, refer_id)
+    
+        return 'True'
+    except:
+        user_id = int(req['user_id'])
+        refer_id = 0
+
+        data.Auth(user_id, refer_id)
+        return 'False'
 
 if __name__ == '__main__':
-    app.run(port=8080, host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
--- static/scripts/Telegram.js
+++ static/scripts/Telegram.js
@@ -1,7 +1,7 @@
 
 var WebApp = window.Telegram.WebApp;
 var userTelegamID = WebApp.initDataUnsafe.user.id;
-//var userReferID = WebApp.initDataUnsafe.start_param;
+var userReferID = WebApp.initDataUnsafe.start_param;
 var personalLink = `https://t.me/share/url?url=t.me/hopterbigbot?startapp=${userTelegamID}`;
 
 function copyLink() {
@@ -14,4 +14,13 @@ function getInvite() {
     window.location.href = personalLink;
 }
 
+function auth() {
+    if (userReferID == undefined) {
+        userReferID = 0;
+    }
 
+    httpRequest = new XMLHttpRequest();
+    httpRequest.open('POST', 'auth');
+    var data = JSON.stringify({"user_id": userTelegamID, "refer_id": userReferID});
+    httpRequest.send(data);
+}
